c06ae3c58c48ea28a555201b055802a37a38fc5b
EgorK2606 <kramere26@gmail.com>
2020-10-12 10:10:22 +0200

    First commit - empty project

---
+++ Chat - App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat - App
+//
+//  Created by Gregor Kramer on 12.10.2020.
+//  Copyright © 2020 Gregor Kramer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
